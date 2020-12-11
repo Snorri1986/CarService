1afb7aed76316ce63c8c2b169165ed63152fce09
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2020-12-11 23:38:08 +0200

    version 0.1.1

--- src/main/java/hello/CarServiceApplication.java
+++ src/main/java/hello/CarServiceApplication.java
@@ -2,10 +2,19 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CarServiceApplication {
+public class CarServiceApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+         return application.sources(CarServiceApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CarServiceApplication.class,args);
     }
 }
+
