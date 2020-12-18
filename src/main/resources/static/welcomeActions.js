25f78845e250f3539e7374df06ecc7090423153d
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2020-12-18 23:19:56 +0200

    version 0.2

---
+++ src/main/resources/static/welcomeActions.js
@@ -0,0 +1,12 @@
+/* JavaScript service file for buttons on index.html */
+/* version 0.2 */
+
+function openLoginPage() {
+window.location.href = "login.html";
+}
+
+function openRegisterPage() {
+window.location.href = "register.html";
+}
+/**/
+
