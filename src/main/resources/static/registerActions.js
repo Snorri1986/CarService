e96aa54206e8ac45ef187689478ddffdb74fac1f
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2020-12-30 22:09:25 +0200

    version 0.2.3

---
+++ src/main/resources/static/registerActions.js
@@ -0,0 +1,11 @@
+/* JavaScript service file for buttons on register.html */
+/* version 0.2.3 */
+
+/* Clear all data from register form fields */
+function eraseAllData() {
+document.getElementById("login").value="";
+document.getElementById("pwd").value="";
+document.getElementById("confirmedPwd").value="";
+document.getElementById("eml").value="";
+}
+/* ... */
