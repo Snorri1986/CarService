1e34e91f947d9fe2fbcf55b8f837f1d7fb734f47
dionisiy1986@gmail.com <dionisiy1986@gmail.com>
2021-01-04 23:42:54 +0200

    version 0.3.1-alpha-04012021

---
+++ src/main/resources/static/loginActions.js
@@ -0,0 +1,9 @@
+/* JavaScript service file for buttons on login.html */
+/* version 0.3.1 */
+
+/* Clear all data from login form fields */
+function eraseAllData() {
+document.getElementById("login").value="";
+document.getElementById("pwd").value="";
+}
+/* ... */
